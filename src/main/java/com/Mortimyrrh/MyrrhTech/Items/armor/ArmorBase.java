a5a0427cb1bb868558beae81f50af3d36cbc8adf
Mortimer Pavlitski <mortimer_@me.com>
2019-10-29 19:41:06 +0000

    added armour nearly

---
+++ src/main/java/com/Mortimyrrh/MyrrhTech/Items/armor/ArmorBase.java
@@ -0,0 +1,29 @@
+package com.Mortimyrrh.MyrrhTech.Items.armor;
+
+import com.Mortimyrrh.MyrrhTech.Main;
+import com.Mortimyrrh.MyrrhTech.Init.ModItems;
+import com.Mortimyrrh.MyrrhTech.util.IHasModel;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.inventory.EntityEquipmentSlot;
+import net.minecraft.item.ItemArmor;
+
+public class ArmorBase extends ItemArmor implements IHasModel {
+
+	public ArmorBase(String name, ArmorMaterial materialIn, int renderIndexIn, EntityEquipmentSlot equipmentSlotIn) {
+		super(materialIn, renderIndexIn, equipmentSlotIn);
+		
+		setUnlocalizedName(name);
+		setRegistryName(name);
+		setCreativeTab(CreativeTabs.COMBAT);
+		
+		ModItems.ITEMS.add(this);
+	}
+
+	@Override
+	public void registerModels() 
+	{
+		Main.proxy.registerItemRenderer(this, 0, "inventory");
+	}
+	
+}
